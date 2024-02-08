506a787d75ec5572eee0533604ee1bdde05a7aa2
Aliya Fatihah <aliya.sidek@hexainitiative.com>
2024-02-08 21:18:02 +0800

    [NodeJs-Auth] - Lesson 4: Mongoose Hooks

--- NodeJS/Projects/node-jwt-auth/models/User.js
+++ NodeJS/Projects/node-jwt-auth/models/User.js
@@ -17,6 +17,19 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// fire a function after doc saved to db
+userSchema.post('save',function(doc,next){
+    console.log('new user was created & saved',doc);
+    next();
+
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user about to be created & saved',this);
+    next();
+});
+
 const User = mongoose.model('user',userSchema); //has to be singular than what we created in collections
 
 module.exports = User;
