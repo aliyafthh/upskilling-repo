db33ef388a9ed2e035edfb13e92a9985b8743d6c
Aliya Fatihah <aliya.sidek@hexainitiative.com>
2024-02-09 14:36:43 +0800

    [NodeJs-Auth] - Lesson 11: Logout flow & Conditional Rendering

--- NodeJS/Projects/node-jwt-auth/controllers/authControllers.js
+++ NodeJS/Projects/node-jwt-auth/controllers/authControllers.js
@@ -74,3 +74,8 @@ module.exports.login_post = async (req,res) =>{
     }
   
 }
+
+module.exports.logout_get = (req,res) =>{
+    res.cookie('jwt', '', {maxAge:1});
+    res.redirect('/');
+}
--- NodeJS/Projects/node-jwt-auth/middleware/authMiddleware.js
+++ NodeJS/Projects/node-jwt-auth/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = function (req,res,next) {
     const token = req.cookies.jwt;
@@ -18,4 +19,26 @@ const requireAuth = function (req,res,next) {
     }
 };
 
-module.exports = {requireAuth};
+//check current user
+const checkUser = async (req,res,next)=>{
+    const token = req.cookies.jwt;
+    if(token) {
+        jwt.verify(token, 'net ninja secret', async (err, decodedToken)=>{
+            if(err){
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            }else{
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    }else{
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = {requireAuth,checkUser};
