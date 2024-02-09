34248a8475480096236789b518f890c5aed4b6a6
Aliya Fatihah <aliya.sidek@hexainitiative.com>
2024-02-09 13:46:37 +0800

    [NodeJs-Auth] - Lesson 9: Login JWT flow; [NodeJs-Auth] - Lesson 10: Protecting Routes

--- NodeJS/Projects/node-jwt-auth/controllers/authControllers.js
+++ NodeJS/Projects/node-jwt-auth/controllers/authControllers.js
@@ -6,6 +6,16 @@ const handleErrors = (err) =>{
     console.log(err.message, err.code);
     let errors = {email:'',password:''};
 
+    //incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = 'that email is not registered';
+    }
+
+    //incorrect password
+    if(err.message === 'incorrect password'){
+        errors.email = 'that password is incorrect';
+    }
+
     //duplicate error code
     if(err.code === 11000) {
         errors.email = 'that email is already registered';
@@ -52,11 +62,15 @@ module.exports.signup_post = async (req,res) =>{
 
 module.exports.login_post = async (req,res) =>{
     const { email, password} = req.body;
-    
+
     try {
-        const user = await User.create({email, password});
-
-    }catch(err){
-
+        const user = await User.login(email,password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({ user: user._id });
+    }catch(e){
+        const errors = handleErrors(e);
+        res.status(400).json({ errors });
     }
+  
 }
